27c6032bf5d5280142f3fc92c15c0e035f638f0c
Rohan Sonar <rohansunar89@gmail.com>
2025-04-28 15:51:07 +0530

    Added pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "maabit-nextjs",
+      script: "npm",
+      args: "start",
+      cwd: __dirname,
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "512M",
+    },
+  ],
+};
